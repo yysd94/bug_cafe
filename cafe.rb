6dd9f62c5f0ebc2b576c590ed9d1b34856bbb1ee
yysd94 <yupianon@gmail.com>
2024-05-22 22:31:25 +0900

    FOODS, DRINKSについて、宣言時にpriceの値を数値リテラルとするように修正しました。

--- cafe.rb
+++ cafe.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
@@ -30,5 +30,5 @@ order_drinks = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order_foods = take_order(FOODS)
 
-total = DRINKS[order_drinks-1][:price].to_i + FOODS[order_foods-1][:price].to_i
+total = DRINKS[order_drinks-1][:price] + FOODS[order_foods-1][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
